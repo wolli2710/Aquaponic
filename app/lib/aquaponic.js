f8f1c31339c1be7109eea2d2a9a20ca4a01c0233
Wolfgang Vogl <wolfgang.vogl01@gmail.com>
2018-08-10 00:10:58 +0200

    Add pump autostart

--- app/lib/aquaponic.js
+++ app/lib/aquaponic.js
@@ -1,8 +1,10 @@
 var feeder = require('./stepper.js').init( [6,13,19,26] );
 var pump = require('./pump.js').init( 23 );
+var config = require('../config/server.js');
 
 var Aquaponic = function(){
-  this.startFeeding = function(){
+
+  var startFeeding = function(){
     // 4076 are 360 degrees
     var counter = 4076 * 20;
     feeder.steps(counter, function(pins) {
@@ -10,22 +12,42 @@ var Aquaponic = function(){
     });
   };
 
-  this.stopFeeding = function(){
+  var stopFeeding = function(){
     feeder.reset();
   };
 
-  this.startPump = function() {
-    var ms = 60 * 1000;
-    pump.start(ms);
+  var startPump = function() {
+    pump.start();
   }
 
-  this.stopPump = function() {
+  var stopPump = function() {
     pump.stop();
   }
+
+  startPumping = function(){
+    var ms = config.pump.duration;
+    var currentMilliseconds = (ms != null) ? ms : (60 * 1000);
+
+    startPump();
+    setTimeout(function () {
+      stopPumping();
+    }, currentMilliseconds);
+  };
+
+  stopPumping = function(){
+    var ms = config.pump.duration;
+    var currentMilliseconds = (ms != null) ? ms : (60 * 1000);
+
+    stopPump();
+    setTimeout(function () {
+      startPumping();
+    }, currentMilliseconds);
+  };
 };
 
 module.exports = {
   init: function(){
+    startPumping();
     return new Aquaponic();
   }
 };
